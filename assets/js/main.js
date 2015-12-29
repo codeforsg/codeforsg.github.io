f7289fea6ff4c4f67349f987b0ee056ed967ce09
Daniel Wong <wonglkd@gmail.com>
2015-12-29 13:20:04 +0000

    Move stuff around, basic config

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// http://stackoverflow.com/a/27091870/1306353
+// any link that is not part of the current domain is modified
+
+(function() {
+  var links = document.links;
+  for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+    // can also be
+    //  links[i].hostname != 'subdomain.example.com'
+    if (links[i].hostname != window.location.hostname) {
+      links[i].target = '_blank';
+      links[i].className += ' externalLink';
+    }
+  }
+})();
